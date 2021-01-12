e9d445be2a99138e727dc4e91258f3cb2a104c93
fvmutlu <43426660+fvmutlu@users.noreply.github.com>
2021-01-12 15:07:44 -0500

    implemented pipage rounding

--- julia/helper_functions.jl
+++ julia/helper_functions.jl
@@ -14,7 +14,7 @@ function projOpt(S_step_t, P_min, P_max, Y_step_t, C, cache_capacity)
     problem = minimize(norm(Y_proj_t - Y_step_t),[Y_proj_t >= 0, Y_proj_t <= 1, C*Y_proj_t <= cache_capacity])
     solve!(problem, SCS.Optimizer)
     
-    return [S_proj_t, Y_proj_t]
+    return [evaluate(S_proj_t), evaluate(Y_proj_t)]
 end
 
 function randomInitPoint(dim_S, dim_Y, P_min, P_max, C, cache_capacity, weight)
@@ -27,10 +27,10 @@ function randomInitPoint(dim_S, dim_Y, P_min, P_max, C, cache_capacity, weight)
     S_0 = rand(pd, dim_S)
 
     # for caching vector
-    mean = 0.5 + 0.5*weight/2;
-    var = 0.1;
+    mean = 0.5 + 0.5*weight/2
+    var = 0.1
     pd = Normal(mean, var)
-    Y_0 = rand(pd,dim_Y);
+    Y_0 = rand(pd,dim_Y)
 
     return projOpt(S_0, P_min, P_max, Y_0, C, cache_capacity)
 end
@@ -39,21 +39,23 @@ function pipageRound(F, Gintegral, Y_opt, S_opt, M, V, cache_capacity)
     Y_matrix = reshape(Y_opt, (M, V)) # put the caching vector into matrix form
     epsilon = 1e-3
     for v in 1:V # repeat for all nodes 
-        y = Y_matrix(:,v); # get node v's caching variables (column vector)
+        y = Y_matrix[:,v]; # get node v's caching variables (column vector)
         y[findall(i->(i<epsilon), y)] .= 0 # fix floating-point rounding errors by rounding all values in epsilon neighborhood of 0
         y[findall(i->(abs(i-1)<epsilon), y)] .= 1 # fix floating-point rounding errors by rounding all values in epsilon neighborhood of 1
         while !(all(isbinary, y)) # repeat as long as there are fractional values
             y_frac_pair_ind = findall(!isbinary, y)
-            if(length(y_frac_pair_ind)==1)
+            if length(y_frac_pair_ind)==1
                 y_temp = y
                 y_temp[y_frac_pair_ind] = 1
-                if(sum(y_temp)<=cache_capacity[v])
+                if sum(y_temp)<=cache_capacity[v]
                     y[y_frac_pair_ind] = 1
                 else
                     y[y_frac_pair_ind] = 0
                 end
             else
-                DO_best = Inf;
+                DO_best = 9999999999.9;
+                y_best = [0.0 0.0]
+
                 for pair_ind_temp in collect(combinations(y_frac_pair_ind, 2))
                     y_frac_pair = y[pair_ind_temp]
                     y1 = y_frac_pair[1]
@@ -61,57 +63,62 @@ function pipageRound(F, Gintegral, Y_opt, S_opt, M, V, cache_capacity)
                     y_temp = y
 
                     # Case 1: Try rounding y1 to 0
-                    if(y1 + y2 < 1)
-                        y_temp[pair_ind_temp] = [0 y1+y2];
-                        Y_temp = Y_matrix;
-                        Y_temp[:,v] = y_temp;
-                        Y_temp = reshape(Y_temp, (M*V, 1));
-                        DO_temp = F(S_opt')*transpose(Gintegral(Y_temp'));
+                    if y1 + y2 < 1
+                        y_temp[pair_ind_temp] = [0 y1+y2]
+                        Y_temp = Y_matrix
+                        Y_temp[:,v] = y_temp
+                        Y_temp = reshape(Y_temp, (M*V, 1))                        
+                        DO_temp = dot(F(S_opt),Gintegral(Y_temp))                        
                         if (DO_temp < DO_best)
-                            DO_best = DO_temp;
-                            y_best = y_temp;
+                            DO_best = DO_temp
+                            y_best = y_temp
                         end
                     end
+
                     # Case 2: Try rounding y1 to 1
-                    if(y2-(1-y1)>0)
-                        y_temp[pair_ind_temp] = [1 y2-(1-y1)];
-                        Y_temp = Y_matrix;
-                        Y_temp[:,v] = y_temp;
-                        Y_temp = reshape(Y_temp, (M*V,1));
-                        DO_temp = F(S_opt')*transpose(Gintegral(Y_temp'));
+                    if y2 - (1 - y1) > 0
+                        y_temp[pair_ind_temp] = [1 y2 - (1 - y1)]
+                        Y_temp = Y_matrix
+                        Y_temp[:,v] = y_temp
+                        Y_temp = reshape(Y_temp, (M*V,1))                        
+                        DO_temp = dot(F(S_opt),Gintegral(Y_temp))                        
                         if (DO_temp < DO_best)
-                            DO_best = DO_temp;
-                            y_best = y_temp;
+                            DO_best = DO_temp
+                            y_best = y_temp
                         end
                     end
+
                     # Case 3: Try rounding y2 to 0
-                    if(y1+y2<1)
-                        y_temp[pair_ind_temp] = [y1+y2 0];
-                        Y_temp = Y_matrix;
-                        Y_temp[:,v] = y_temp;
-                        Y_temp = reshape(Y_temp, (M*V,1));
-                        DO_temp = F(S_opt')*transpose(Gintegral(Y_temp'));
+                    if y1 + y2 < 1
+                        y_temp[pair_ind_temp] = [y1+y2 0]
+                        Y_temp = Y_matrix
+                        Y_temp[:,v] = y_temp
+                        Y_temp = reshape(Y_temp, (M*V,1))                        
+                        DO_temp = dot(F(S_opt),Gintegral(Y_temp))
                         if (DO_temp < DO_best)
-                            DO_best = DO_temp;
-                            y_best = y_temp;
+                            DO_best = DO_temp
+                            y_best = y_temp
                         end
                     end
+
                     # Case 4: Try rounding y2 to 1
-                    if(y1-(1-y2)>0)
-                        y_temp[pair_ind_temp] = [y1-(1-y2) 1];
-                        Y_temp = Y_matrix;
-                        Y_temp[:,v] = y_temp;
-                        Y_temp = reshape(Y_temp, (M*V,1));
-                        DO_temp = F(S_opt')*transpose(Gintegral(Y_temp'));
+                    if y1 - (1 - y2) > 0
+                        y_temp[pair_ind_temp] = [y1-(1-y2) 1]
+                        Y_temp = Y_matrix
+                        Y_temp[:,v] = y_temp
+                        Y_temp = reshape(Y_temp, (M*V,1))                        
+                        DO_temp = dot(F(S_opt),Gintegral(Y_temp))                        
                         if (DO_temp < DO_best)
-                            DO_best = DO_temp;
-                            y_best = y_temp;
+                            DO_best = DO_temp
+                            y_best = y_temp
                         end
                     end
                 end
-                y = y_best; # If no cases had better than the current objective, this will just pick the best result out of all trials
+                y = round.(y_best); # If no cases had better than the current objective, this will just pick the best result out of all trials.
+                # We round because the results from projection can be arbitrarily close to 1 or 0 while they should be exactly 1 or 0 (e.g. 0.9999999875 or 4.57987363e-11), I suspect this is due to inner workings of Convex.jl
             end
         end
-        Y_matrix[:,v] = y;
+        Y_matrix[:,v] = y
     end
-    X = reshape(Y_matrix, (M*V,1));
+    return reshape(Y_matrix, (M*V,1))
+end
--- julia/opt_test.jl
+++ julia/opt_test.jl
@@ -3,12 +3,36 @@ include("helper_functions.jl")
 P_min = 1
 P_max = 10
 
-dim_S = 2
-dim_Y = 4
+M = 2
+V = 1
 
-cache_capacity = [1; 2]
-C = [1 1 0 0; 0 0 1 1]
+dim_S = 3
+dim_Y = 2
+
+cache_capacity = 1
+C = [1 1]
 weight = 0.5
 
 (S_0, Y_0) = randomInitPoint(dim_S, dim_Y, P_min, P_max, C, cache_capacity, weight)
-(S_proj_t, Y_proj_t) = projOpt(S_0, P_min, P_max, Y_0, C, cache_capacity)
+println(S_0)
+println(Y_0)
+
+function F(S)
+    lambda1 = 1.2
+    lambda2 = 1.5
+    sinr1 = S[1]/(1+S[2]+S[3]/16)
+    sinr2 = S[2]/(1+S[1]+S[3]/16)
+    sinr3 = S[3]
+    f1 = 1/log(1+sinr1)
+    f2 = 1/log(1+sinr2)
+    f3 = 1/log(1+sinr3)
+    return [lambda1*f1 lambda1*f3 lambda2*f2 lambda2*f3]
+end
+
+function Gintegral(Y)
+    g1 = 1 - Y[1]
+    g2 = 1 - Y[2]
+    return [1 g1 1 g2]
+end
+
+pipageRound(F, Gintegral, Y_0, S_0, M, V, cache_capacity)
