ec56891b34883b431b841d05c058acaca0c84ee0
fvmutlu <43426660+fvmutlu@users.noreply.github.com>
2021-02-04 15:47:22 -0500

    Modularized simulation running functions

--- julia/methods.jl
+++ julia/methods.jl
@@ -245,3 +245,113 @@ function altMethod(SY_0, funcs, consts)
     end
     return (D_best_alt, S_best_alt, Y_best_alt)
 end
+
+function altMethodMult(SY_0, funcs, consts)
+    numof_initpoints = length(SY_0)
+    F = funcs.F
+    grad_S_F = funcs.grad_S_F
+    G = funcs.G
+    subgrad_Y_G = funcs.subgrad_Y_G
+
+    D_alt_arr = Array{Float64}(undef, numof_initpoints)
+    S_alt_arr = Array{Array{Float64,1}}(undef, numof_initpoints)
+    Y_alt_arr = Array{Array{Float64,1}}(undef, numof_initpoints)
+    Threads.@threads for (i, (S_0, Y_0)) in collect(enumerate(SY_0))
+        D_0 = sum([ F[m](S_0) for m in 1:length(F) ] .* [ G[n](Y_0) for n in 1:length(G) ])
+        S_t = S_0
+        Y_t = Y_0
+        S_t_prev = S_t
+        Y_t_prev = Y_t
+        D_t = D_0
+
+        D_best_iter_alt = Inf
+        D_best_iter_prev_alt = Inf
+        S_best_iter_alt = S_t
+        Y_best_iter_alt = Y_t
+        iter = 0
+        while iter == 0 || abs(D_best_iter_prev_alt - D_best_iter_alt) >= epsilon # Outer loop for alternating method
+            D_hat_t = Inf
+            delta = D_t/2
+            div_ctr = 0
+            dim_val = 2
+            slow_ctr = 0
+            t = 0
+            while t==0 || ( (abs(D_hat_t - D_t) >= epsilon) && (norm(S_t_prev - S_t) >= epsilon_S) )
+                D_hat_t = min(D_t, D_hat_t) # If current objective is the minimum so far, replace D_hat
+                d_S_t = hcat([ grad_S_F[m](S_t) for m in 1:length(grad_S_F) ]...) * [ G[n](Y_t) for n in 1:length(G) ] # Gradient of D w.r.t S evaluated at (Yₜ,Sₜ)
+                step_size_S = (D_t - D_hat_t + delta) / (norm(d_S_t)^2) # Polyak step size calculation
+                S_step_t = S_t - step_size_S*d_S_t # Take step for S
+                S_proj_t, Y_proj_t = projOpt(S_step_t, Y_t, consts); # Projection (ignore Y_proj_t for power step)
+                S_t_prev = S_t # We need to save S^t for the while condition
+                S_t = S_proj_t # S^{t+1} = \bar{S}^t
+                D_t = sum([ F[m](S_t) for m in 1:length(F) ] .* [ G[n](Y_t) for n in 1:length(G) ]) # Calculate the objective for iteration t
+                if D_t > D_hat_t # Decrease delta when zigzagging occurs
+                    div_ctr += 1
+                    if div_ctr==5
+                        delta = delta / sqrt(dim_val)
+                        dim_val += 1
+                        div_ctr = 0
+                        slow_ctr = 0
+                    end
+                elseif D_hat_t - D_t <= 10*epsilon
+                    slow_ctr += 1
+                    if slow_ctr==5  # Increase delta when convergence is slow
+                        delta = delta * sqrt(dim_val)
+                        slow_ctr = 0
+                        div_ctr = 0
+                    end
+                end
+                t += 1
+            end
+
+            # Reset variables when moving to caching optimization round
+            D_hat_t = Inf
+            delta = D_t/2
+            div_ctr = 0
+            dim_val = 2
+            slow_ctr = 0
+            t = 0
+            while t==0 || ( (abs(D_hat_t - D_t) >= epsilon) && (norm(Y_t_prev - Y_t) >= epsilon_Y) )
+                D_hat_t = min(D_t, D_hat_t) # If current objective is the minimum so far, replace D_hat
+                d_Y_t = hcat([ subgrad_Y_G[m](Y_t) for m in 1:length(subgrad_Y_G) ]...) * ([ F[n](S_t) for n in 1:length(F) ]) # Subgradient of D w.r.t Y evaluated at (Y_t,S_t)
+                step_size_Y = (D_t - D_hat_t + delta) / (norm(d_Y_t)^2) # Polyak step size calculation
+                Y_step_t = Y_t - step_size_Y*d_Y_t # Take step for Y
+                S_proj_t, Y_proj_t = projOpt(S_t, Y_step_t, consts); # Projection (ignore S_proj_t for caching step)
+                Y_t_prev = Y_t # We need to save Y^t for the while condition
+                Y_t = Y_proj_t # Y^{t+1} = \bar{Y}^t
+                D_t = sum([ F[m](S_t) for m in 1:length(F) ] .* [ G[n](Y_t) for n in 1:length(G) ]) # Calculate the objective for iteration t
+                if D_t > D_hat_t # Decrease delta when zigzagging occurs
+                    div_ctr += 1
+                    if div_ctr==5
+                        delta = delta / sqrt(dim_val)
+                        dim_val += 1
+                        div_ctr = 0
+                        slow_ctr = 0
+                    end
+                elseif D_hat_t - D_t <= 10*epsilon
+                    slow_ctr += 1
+                    if slow_ctr==5  # Increase delta when convergence is slow
+                        delta = delta * sqrt(dim_val)
+                        slow_ctr = 0
+                        div_ctr = 0
+                    end
+                end
+                t += 1
+            end
+            D_best_iter_prev_alt = D_best_iter_alt
+            if D_t < D_best_iter_alt # Set new best point for this initial point
+                D_best_iter_alt = D_t
+                S_best_iter_alt = S_t
+                Y_best_iter_alt = Y_t
+            end
+            iter += 1
+        end
+        D_alt_arr[i] = D_best_iter_alt
+        S_alt_arr[i] = S_best_iter_alt
+        Y_alt_arr[i] = Y_best_iter_alt
+    end
+    D_best_alt = minimum(D_alt_arr)
+    S_best_alt = S_alt_arr[argmin(D_alt_arr)]
+    Y_best_alt = Y_alt_arr[argmin(D_alt_arr)]
+    return (D_best_alt, S_best_alt, Y_best_alt)
+end
--- julia/sim_functions.jl
+++ julia/sim_functions.jl
@@ -1,8 +1,8 @@
 include("methods.jl")
-import Base.Threads.@spawn
+
 using Printf
 
-## Constraint parameters
+#= ## Constraint parameters
 c_mc = 4 # Cache capacity of macro cell in number of items
 c_sc = 2 # Cache capacity of small cell in number of items
 P_min = 1 # Minimum allowed power on each transmission edge
@@ -14,7 +14,7 @@ SC = 4 # Number of small cells
 U = V - SC - 1 # Number of users
 R_cell = 4.0 # Radius of cell area
 pathloss_exponent = 4.0 # Rate at which the received signal strength decreases with distance
-noise = 1.0 # White noise within cell area
+noise = 1.0 # White noise power within cell area
 C_mc = (R_cell/2)^pathloss_exponent # Cost at the wireline edge between backhaul to macro cell (calculated completely heuristically, there could be smarter way of determining this)
 C_sc = 1.5 * C_mc # Cost at the wireline edge between backhaul and any of the small cells (again, heuristic)
 D_bh_mc = 1 / log( 1 + ((1/C_mc)*P_max)/noise ) # Delay of data retrieval from backhaul to macro cell (again, computed heuristically using the wireline edge costs)
@@ -35,43 +35,86 @@ Cmat = zeros(Int64, V,M*V) # For cache capacity constraint C*Y <= cache_capacity
 for n in 1:V
     Cmat[ n, (n-1)*M+1 : n*M ] .= 1 # In matrix C, mark entries corresponding to node n's items as 1
 end
-consts = (P_min = P_min, P_max = P_max, cache_capacity = cc, C = Cmat)
+consts = (P_min = P_min, P_max = P_max, cache_capacity = cc, C = Cmat) =#
+
+
+function readConfig(input = "/home/volkan/opt-caching-power/julia/config.txt")
+    cfg = split.(readlines(input), " # ")
+    (V, SC, M, c_mc, c_sc, pd_type) = parse.(Int64, [ cfg[i][1] for i in 1:6 ])
+    (P_min, P_max, pathloss_exponent, noise, R_cell, pd_param) = parse.(Float64, [ cfg[i][1] for i in 7:12 ])
+    
+    U = V - SC - 1
+    C_mc = (R_cell/2)^pathloss_exponent # Cost at the wireline edge between backhaul to macro cell (calculated completely heuristically, there could be smarter way of determining this)
+    C_sc = 1.5 * C_mc # Cost at the wireline edge between backhaul and any of the small cells (again, heuristic)
+    D_bh_mc = 1 / log( 1 + ((1/C_mc)*P_max)/noise ) # Delay of data retrieval from backhaul to macro cell (again, computed heuristically using the wireline edge costs)
+    D_bh_sc = 1 / log( 1 + ((1/C_sc)*P_max*0.8)/noise ) # Delay of data retrieval from backhaul to any of the small cells (again, computed heuristically using the wireline edge costs)
+    
+    if pd_type == 0
+        pd = (1 ./ (1:M).^ pd_param) / sum(1 ./ (1:M).^ pd_param)
+    else # have different distributions here possibly?
+        pd = (1 ./ (1:M).^ pd_param) / sum(1 ./ (1:M).^ pd_param)
+    end
+
+    params = (noise = noise, numof_requests = U, V = V, M = M, pd = pd, D_bh_mc = D_bh_mc, D_bh_sc = D_bh_sc)
+    netparams = (V = V, SC = SC, R_cell = R_cell, pathloss_exponent = pathloss_exponent, C_mc = C_mc, C_sc = C_sc)
+    constparams = (P_min = P_min, P_max = P_max, c_mc = c_mc, c_sc = c_sc)
+
+    return params, netparams, constparams
+end
+
+function newConsts(params = params, netgraph = netgraph, constparams = constparams)
+    V = params.V
+    M = params.M
+
+    cc = zeros(Int64, V,1) # For cache capacity constraint C*Y <= cache_capacity
+    cc[1] = constparams.c_mc
+    cc[netgraph.sc_nodes, 1] .= constparams.c_sc
+    Cmat = zeros(Int64, V,M*V) # For cache capacity constraint C*Y <= cache_capacity
+    for n in 1:V
+        Cmat[ n, (n-1)*M+1 : n*M ] .= 1 # In matrix C, mark entries corresponding to node n's items as 1
+    end
+
+    consts = (P_min = constparams.P_min, P_max = constparams.P_max, cache_capacity = cc, C = Cmat)
+    return consts
+end
+
+function newGraph(params = params, netparams = netparams, constparams = constparams)
+    netgraph = cellGraph(netparams.V, netparams.SC, netparams.R_cell, netparams.pathloss_exponent, netparams.C_mc, netparams.C_sc)
+
+    funcs = funcSetup(netgraph, params)
+
+    consts = newConsts(params, netgraph, constparams)
+
+    return netgraph, funcs, consts
+end
 
 ## Run optimization over different initial points
 
-function runSim(numof_edges, numof_initpoints, funcs, consts, params)
-    weight = (i, N) -> ( i - ( (N + 1) / 2) ) / (N - 1)
-    SY_0 = [ randomInitPoint(numof_edges, M*V, weight(i, numof_initpoints), consts) for i in 1:numof_initpoints ]
+#function runSim(numof_edges, numof_initpoints, funcs, consts, params)
+function runSim(numof_initpoints, numof_edges = size(netgraph.edges,1), params = params, consts = consts, funcs = funcs)
+    weight = (i, N) -> (N != 1) * (( i - ( (N + 1) / 2) ) / (N - 1))
+    SY_0 = [ randomInitPoint(numof_edges, params.M*params.V, weight(i, numof_initpoints), consts) for i in 1:numof_initpoints ]
+
     println(" -- SUB -- ")
     (D_opt, S_opt, Y_opt) = @time subMethod(SY_0, funcs, consts)
     X_opt = pipageRound(funcs.F, funcs.Gintegral, S_opt, Y_opt, params.M, params.V, consts.cache_capacity)
     D_0 = sum([ funcs.F[m](S_opt) for m in 1:length(funcs.F) ] .* [ funcs.Gintegral[n](X_opt) for n in 1:length(funcs.G) ])
     @printf("Relaxed delay: %.2f || Rounded delay: %.2f\n", D_opt, D_0)
-    println(" -- SUBMULT -- ")
+
+    println(" -- SUB MULT -- ")
     (D_opt, S_opt, Y_opt) = @time subMethodMult(SY_0, funcs, consts)
     X_opt = pipageRound(funcs.F, funcs.Gintegral, S_opt, Y_opt, params.M, params.V, consts.cache_capacity)
     D_0 = sum([ funcs.F[m](S_opt) for m in 1:length(funcs.F) ] .* [ funcs.Gintegral[n](X_opt) for n in 1:length(funcs.G) ])
     @printf("Relaxed delay: %.2f || Rounded delay: %.2f\n", D_opt, D_0)
+
     println(" -- ALT --")
     (D_opt, S_opt, Y_opt) = @time altMethod(SY_0, funcs, consts)
     X_opt = pipageRound(funcs.F, funcs.Gintegral, S_opt, Y_opt, params.M, params.V, consts.cache_capacity)
     D_0 = sum([ funcs.F[m](S_opt) for m in 1:length(funcs.F) ] .* [ funcs.Gintegral[n](X_opt) for n in 1:length(funcs.G) ])
     @printf("Relaxed delay: %.2f || Rounded delay: %.2f\n", D_opt, D_0)
-end
 
-function runSimMult(numof_edges, numof_initpoints, funcs, consts, params)
-    weight = (i, N) -> ( i - ( (N + 1) / 2) ) / (N - 1)
-    SY_0 = [ randomInitPoint(numof_edges, M*V, weight(i, numof_initpoints), consts) for i in 1:numof_initpoints ]
-    println(" -- Simulation begins -- ")
-    t1 = @spawn subMethod(SY_0, funcs, consts)
-    t2 = @spawn altMethod(SY_0, funcs, consts)
-    println(" -- SUB -- ")
-    (D_opt, S_opt, Y_opt) = fetch(t1)
-    X_opt = pipageRound(funcs.F, funcs.Gintegral, S_opt, Y_opt, params.M, params.V, consts.cache_capacity)
-    D_0 = sum([ funcs.F[m](S_opt) for m in 1:length(funcs.F) ] .* [ funcs.Gintegral[n](X_opt) for n in 1:length(funcs.G) ])
-    @printf("Relaxed delay: %.2f || Rounded delay: %.2f\n", D_opt, D_0)
-    println(" -- ALT -- ")
-    (D_opt, S_opt, Y_opt) = fetch(t2)    
+    println(" -- ALT MULT -- ")
+    (D_opt, S_opt, Y_opt) = @time altMethodMult(SY_0, funcs, consts)
     X_opt = pipageRound(funcs.F, funcs.Gintegral, S_opt, Y_opt, params.M, params.V, consts.cache_capacity)
     D_0 = sum([ funcs.F[m](S_opt) for m in 1:length(funcs.F) ] .* [ funcs.Gintegral[n](X_opt) for n in 1:length(funcs.G) ])
     @printf("Relaxed delay: %.2f || Rounded delay: %.2f\n", D_opt, D_0)
